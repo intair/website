f1b017e9c395616e62a1711f97f4cf3620d0ff0f
intair <intair@crime.su>
2021-06-11 23:03:51 -0400

    ss

--- assets/js/slider.js
+++
@@ -1,9 +0,0 @@
-//var music = document.getElementById('music');
-///var volumeControl = document.getElementById('vol-control');
-
-//var setVolume = function () {
-//	music.volume = this.value / 50;
-//};
-
-//volumeControl.addEventListener('change', setVolume);
-//volumeControl.addEventListener('input', setVolume);
